a0e721a0e8e601c107cecf70238d5c99d44f8cb0
Henrique Batista <wellington.h.batista@hotmail.com>
2022-11-21 07:46:00 -0300

    First Commit created API FaceBook Clone

---
+++ start/routes/auth.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.resource('/auth', 'Auth/Main').only(['store', 'destroy']).middleware({
+  destroy: ['auth']
+})
