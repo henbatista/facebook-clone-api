9dbe96cae67cb295ddc271246545126ec77b6a29
Henrique Batista <wellington.h.batista@hotmail.com>
2022-11-25 19:17:31 -0300

    Created Update Validator

--- app/Controllers/Http/Users/Register.ts
+++ app/Controllers/Http/Users/Register.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { StoreValidator } from 'App/Validators/User/Register'
+import { StoreValidator, UpdateValidator } from 'App/Validators/User/Register'
 import { User, UserKey } from 'App/Models'
 import { faker } from '@faker-js/faker'
 import Mail from '@ioc:Adonis/Addons/Mail'
@@ -34,5 +34,20 @@ export default class UserRegistersController {
     return user
   }
 
-  public async update({}: HttpContextContract) {}
+  public async update({ request, response }: HttpContextContract) {
+    const { key, name, password } = await request.validate(UpdateValidator)
+    const userKey = await UserKey.findByOrFail('key', key)
+    const user = await userKey.related('user').query().firstOrFail()
+
+    const username = name.split('')[0].toLocaleLowerCase() + new Date().getTime()
+
+    user.merge({name, password, username})
+
+    await user.save()
+
+    await userKey.delete()
+
+    return response.ok({ message: 'ok'})
+
+  }
 }
--- start/routes/auth.ts
+++ start/routes/auth.ts
@@ -1,6 +1,5 @@
 /* eslint-disable prettier/prettier */
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.resource('/auth', 'Auth/Main').only(['store', 'destroy']).middleware({
-  destroy: ['auth']
-})
+Route.post('/auth', 'Auth/Main')
+Route.delete('/auth', 'Auth/Main.destroy').middleware('auth')
