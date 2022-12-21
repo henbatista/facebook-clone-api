5892a638d39d4b2fd91e52fb9ec643b00a0a9f11
Henrique Batista <wellington.h.batista@hotmail.com>
2022-12-21 19:39:01 -0300

    Created Migrations and routes for posts

---
+++ app/Validators/Post/Main/StoreValidator.ts
@@ -0,0 +1,14 @@
+import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class StoreValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    description: schema.string.optional({ trim: true }),
+  })
+
+  public cacheKey = this.ctx.routeKey
+
+  public messages: CustomMessages = {}
+}
