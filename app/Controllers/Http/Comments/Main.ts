e5f6d7ef2d720624e33e1a8da8149447c4b83776
Henrique Batista <wellington.h.batista@hotmail.com>
2022-12-23 08:59:38 -0300

    Created model and migration Comments

---
+++ app/Controllers/Http/Comments/Main.ts
@@ -0,0 +1,9 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class MainsController {
+  public async store({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+}
